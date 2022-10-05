7e388a8121d3203d49e897a4e21518ff986b6649
Fromredtoviolet <aprilartists@gmail.com>
2022-10-05 21:50:26 +0900

    수업 진행

--- java_demo01/src/kr/co/demo/Demo01.java
+++ java_demo01/src/kr/co/demo/Demo01.java
@@ -1,11 +1,24 @@
 package kr.co.demo;
 
+/**
+ * 클래스나 메서드에 대한 설명을 작성하기 위한 용도로 사용
+ * 하지만 블럭 주석이다.
+ * @author user2 
+ *
+ */
 public class Demo01 {
 
+	/*
+	 * 블럭 주석
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 인라인 주석
+		
+		// 출력 코드 : 콘솔(터미널)에 문자열이 출력된다.
 		System.out.print("Hello World!!");
-	
+		
+		// 실행 코드의 마지막에는 반드시 세미콜론(;)이 있어야 한다.
+		
 	}
 
 }
