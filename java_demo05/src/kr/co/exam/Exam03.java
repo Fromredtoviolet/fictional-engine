e66c6c7f8a06de3c03df6bd117222d7b8d22859c
Fromredtoviolet <aprilartists@gmail.com>
2022-10-13 19:20:43 +0900

    배열 예제

--- java_demo05/src/kr/co/exam/Exam03.java
+++ java_demo05/src/kr/co/exam/Exam03.java
@@ -13,15 +13,20 @@ public class Exam03 {
 		 */
 		Scanner sc = new Scanner(System.in);
 		int size = 0;
+		// size라는 정수의 변수 선언
 		
 		while(!(size >= 2 && size <= 5)) {
 			System.out.print("2 ~ 5 사이의 정수 입력 : ");
 			size = sc.nextInt();
 		}
+		// while 반복문. size라는 정수변수의 범위가 2~5가 아닐때, 사용자 입력을 받는다.
+		// 이 경우 0으로 초기화되어 있으므로 2~5 범위를 충족 못하기 때문에 사용자 입력을 받게 된다.
 		
 		int arr1[] = new int[size];
+		// arr1 정수배열 선언. 크기는 size라는 변수와 같다.
+		// 즉, arr1의 배열 크기는 위에서 사용자가 입력한 size값과 같다.
 		
-		for(int i = 0; i < size;) {
+		for(int i = 0; i < size;) {		// for 반복문. 배열의 칸마다 값 넣기 위해.
 			System.out.print("1 ~ 10 사이의 정수 입력 : ");
 			int num = sc.nextInt();
 			
@@ -31,7 +36,7 @@ public class Exam03 {
 				i++;
 			} else {
 				System.out.println("값을 다시 확인해서 입력하세요.");
-			}
+			}  
 		}
 		for(int i = 0; i < size; i++) {
 			System.out.printf("%d\t", arr1[i]);
--- java_demo05/src/kr/co/exam/Exam04.java
+++ java_demo05/src/kr/co/exam/Exam04.java
@@ -25,17 +25,19 @@ public class Exam04 {
 			int num = sc.nextInt();
 			
 			if(num >= 1 && num <= 10) {
-				boolean isDup = false;
-				for(int j = 1; j <= i; j++) {
-					if(arr1[j] == num) {
-						isDup = true;
-						break;
+				boolean 중복이 = false;	// 중복인지 아닌지 판별하기 위한 불린.
+				for(int j = 0; j < i; j++) {	// 2번 칸에 입력해야할때 1번 칸까지 중복 체크.
+					if(arr1[j] == num) {		// 칸 안의 값이 사용자 입력값과 같느냐?
+						중복이 = true;		// 같으면(중복되면) 참으로 기록해라.
+						break;					// 이 if 구문을 종료하고 빠져나가라.
 					}
 				}
-				if(!isDup) {
+				if(!중복이) { // !를 쓴 부정문이기 때문에, "중복이 아니면 실행"으로 해석
 						System.out.printf("%d 위치에 %d 값으로 초기화하였습니다.\n", i, num);
 						arr1[i] = num;
 						i++;
+				} else { // 위에서 true였으면(중복값이면) 이 구문을 실행하라.
+					System.out.println("이미 초기화된 값입니다.");
 				}
 			} else {
 				System.out.println("값을 다시 확인해서 입력하세요.");
