5df4c58ccba4287c1785a6a57587f19d5ea597f1
Fromredtoviolet <aprilartists@gmail.com>
2022-10-18 21:31:19 +0900

    3번 예제-메뉴 클래스

---
+++ java_demo06/src/kr/co/Exam03/Menu.java
@@ -0,0 +1,26 @@
+package kr.co.Exam03;
+
+public class Menu {
+	private Food food[];
+	
+	public void add() {
+		new Food("떡볶이", 3000);
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void delete() {
+		
+	}
+	
+	public void find() {
+		
+	}
+	
+	
+	
+	
+
+}
