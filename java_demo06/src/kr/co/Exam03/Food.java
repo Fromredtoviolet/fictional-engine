97ba5df2baa67a8b49ea39fd7518387069e769fb
Fromredtoviolet <aprilartists@gmail.com>
2022-10-18 21:31:29 +0900

    3번 예제-푸드 클래스

---
+++ java_demo06/src/kr/co/Exam03/Food.java
@@ -0,0 +1,11 @@
+package kr.co.Exam03;
+
+public class Food {
+	private String name;
+	private int price;
+	
+	public Food(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+}
