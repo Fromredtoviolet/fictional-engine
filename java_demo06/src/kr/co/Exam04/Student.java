48d2d4e7c2197d84cba55a5990998803f9fd9763
Fromredtoviolet <aprilartists@gmail.com>
2022-10-19 21:47:53 +0900

    객체 4번 예제-학생 클래스

---
+++ java_demo06/src/kr/co/Exam04/Student.java
@@ -0,0 +1,13 @@
+package kr.co.Exam04;
+
+public class Student {
+	private Subject subject[];	// 과목클래스 배열
+	private String stuName;		// 학생이름
+	private String stuGrade;	// 학년
+	
+	public Student() {
+		this.subject = new Subject[0];
+		
+	}
+
+}
