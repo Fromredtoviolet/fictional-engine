3a0cab8e83b212626f9573768e9bd193aa887415
Fromredtoviolet <aprilartists@gmail.com>
2022-11-01 21:20:21 +0900

    List 계열 컬렉션

---
+++ java_demo11/src/kr/co/demo01/Subject.java
@@ -0,0 +1,41 @@
+package kr.co.demo01;
+
+public class Subject implements Comparable<Subject> {
+
+	private String name;
+	private double score;
+	
+	public Subject(String name, double score) {
+		this.name = name;
+		this.score = score;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	@Override
+	public String toString() {
+		return "Subject [name=" + name + ", score=" + score + "]";
+	}
+	@Override
+	public int compareTo(Subject o) { 
+		if(this.score > o.score) {
+			// 양수를 반환하면 오름차순, 음수를 반환하면 내림차순
+			return 1;
+		} else if(this.score < o.score) {
+			return -1;
+		}
+		return 0; // 두 스코어가 같은 경우는 0을 반환
+	} 
+
+
+	
+}
