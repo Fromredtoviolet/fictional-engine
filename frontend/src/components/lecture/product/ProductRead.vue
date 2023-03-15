a354e5ba51517d2b1d7c124d4d26230680e666f7
Fromredtoviolet <aprilartists@gmail.com>
2023-03-15 21:41:08 +0900

    feat: [프론트] 상품게시판 글읽기에 이미지 업로드 기능 추가중 (완성X)

--- frontend/src/components/lecture/product/ProductRead.vue
+++ frontend/src/components/lecture/product/ProductRead.vue
@@ -26,6 +26,12 @@
         </td>
       </tr>
       <tr>
+        <td>상품 사진</td>
+        <td>
+          <input type="image" cols="50" :value="product.files" readonly/>
+        </td>
+      </tr>
+      <tr>
         <td>본문</td>
         <td>
           <textarea cols="50" rows="20" :value="product.content" readonly/>
@@ -53,5 +59,5 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 </style>
--- frontend/src/store/states.js
+++ frontend/src/store/states.js
@@ -1,6 +1,8 @@
 export default {
     boards: [],
     board: {},
+    
     products: [],
     product: {},
+    productImages: [],
 } // 낱개가 중괄호, 리스트가 대괄호
