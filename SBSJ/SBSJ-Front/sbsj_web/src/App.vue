a9a965511f18d198daff0a4698865113407dd90d
Fromredtoviolet <aprilartists@gmail.com>
2023-03-21 19:27:42 +0900

    init: [프론트] 상부3조 프론트 저장소 재초기화

---
+++ SBSJ/SBSJ-Front/sbsj_web/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <header/> <!-- 건호씨가 만든 상단 내비 바 자리-->
+    <v-content> <!-- 바디 -->
+      <router-view/>
+    </v-content>
+    <Footer/>
+  </v-app>
+</template>
+
+<script>
+// import Header from ~~ 내비 바 임포트
+import Footer from '@/views/layoutForm/Footer.vue';
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+  components: {
+    Footer
+  }
+};
+</script>
---
+++ SBSJ/SBSJ-Front/sbsj_web/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <div id="home">
+      <div class="elevation-1">
+        캐러셀<br>
+        캐러셀<br>
+        캐러셀<br>
+        캐러셀<br>
+        캐러셀<br>
+        캐러셀<br>
+      </div>
+      <router-view/>
+    </div>
+  </v-container>
+</template>
+
+<script>
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+      
+    },
+  }
+</script>
