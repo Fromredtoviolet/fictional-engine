23f6834a7536921435bef0ab43bd8f81c03c6189
Fromredtoviolet <aprilartists@gmail.com>
2023-04-13 20:16:41 +0900

    백엔드 백업 (4월13일)

---
+++ SBSJ/SBSJ-Back/sbsj_process/src/main/java/com/example/sbsj_process/order/entity/OrderInfo.java
@@ -0,0 +1,29 @@
+package com.example.sbsj_process.order.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+//@Entity
+//@Getter
+//@NoArgsConstructor
+public class OrderInfo {
+
+    // 오더 아이디 (pk)
+
+    // 주문번호
+
+    // 주문자
+
+    // 주문날짜
+
+    // 총 결제금액?
+
+    // 주문상품명
+
+    // 주문상품 수량
+
+
+
+}
