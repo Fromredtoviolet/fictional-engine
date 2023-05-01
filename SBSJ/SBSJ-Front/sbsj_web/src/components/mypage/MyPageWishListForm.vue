557cf85e306eb02c8d7d53a00b9eacb5babd921a
Fromredtoviolet <aprilartists@gmail.com>
2023-05-02 02:46:52 +0900

    프론트 백업 (5월 2일)

--- SBSJ/SBSJ-Front/sbsj_web/src/components/mypage/MyPageWishListForm.vue
+++ SBSJ/SBSJ-Front/sbsj_web/src/components/mypage/MyPageWishListForm.vue
@@ -3,54 +3,53 @@
         <div v-if="!wishList || (Array.isArray(wishList) && wishList.length === 0)">
             <v-card outlined flat height="300">
                 <div class="d-flex justify-center align-center h2 empty-msg">
-                    주문 내역이 존재하지 않습니다.
+                    찜한 상품이 존재하지 않습니다.
                 </div>
             </v-card>
         </div>
         <div v-else>
             <v-row>
-                <v-col v-for="wish in wishList" :key="wish.wishId" cols="3">
-                    <v-card class="mb-5 mx-5 rounded-xl p-4" max-width="300" flat outlined>
-                        <v-checkbox
-                            color="#692498"
-                            hide-details
-                        ></v-checkbox>
-                        <router-link :to="{ name: 'ProductReadPage', params: { productId: wish.productId.toString() } }" class="baby-product-link">
-                            <v-img :src="require(`@/assets/productImgs/${wish.thumbnail}`)" 
-                                contain width="100" height="100" style="border-radius: 20px;"/>
-                        </router-link>
-
-                        <router-link :to="{ name: 'ProductReadPage', params: { productId: wish.productId.toString() } }"
-                            class="baby-product-link" style="text-decoration: none; color: black">
-                            <v-card-title style="font-size: 20px; padding-top:40px;">
-                                <strong>{{ wish.productName }}</strong>
-                            </v-card-title>
-                            
-                            <v-card-text style="font-size: 18px;">
-                                {{ new Intl.NumberFormat().format(wish.price) }}원
-                            </v-card-text>
-                        </router-link>
-
-                        <div class="other-info">
-                            <v-btn class="directive-btn" 
-                                rounded
+                <v-col v-for="wish in wishList" :key="wish.wishId" cols="4" class="d-flex align-items-center">
+                    <v-card class="justify-center mb-5 mx-3 rounded-xl p-4" width="300" height="370" flat outlined>
+                        <div>
+                            <v-checkbox
                                 color="#692498"
-                                outlined
-                                @click="addToCart(wish)"
-                            >
-                                <span class="directive-btn-text">장바구니</span>
-                            </v-btn>
-                            <v-btn
-                                class="directive-btn"
-                                rounded
-                                color="#692498"
-                                dark
-                                @click="directPurchase(wish)"
-                            >
-                                <span class="directive-btn-text">바로구매</span>
-                            </v-btn>
+                                hide-details
+                            ></v-checkbox>
                         </div>
-
+                        <div class="d-flex justify-center">
+                            <router-link :to="{ name: 'ProductReadPage', params: { productId: wish.productId.toString() } }" class="baby-product-link">
+                                <v-img :src="require(`@/assets/productImgs/${wish.thumbnail}`)" 
+                                    class="align-center justify-center"
+                                    contain width="150" height="150"/>
+                            </router-link>
+                        </div>
+                        <v-card-subtitle class="pt-4 justify-center">
+                            <div class="d-flex justify-center">
+                                <router-link :to="{ name: 'ProductReadPage', params: { productId: wish.productId.toString() } }"
+                                    class="baby-product-link" style="text-decoration: none; color: black">
+                                        <strong>{{ wish.productName }}</strong>
+                                </router-link>
+                            </div>
+                            <div class="d-flex justify-center">
+                                <router-link :to="{ name: 'ProductReadPage', params: { productId: wish.productId.toString() } }"
+                                    class="baby-product-link" style="text-decoration: none; color: black">
+                                        {{ new Intl.NumberFormat().format(wish.price) }}원
+                                </router-link>
+                            </div>
+                        </v-card-subtitle>
+                        <v-card-actions class="justify-center">
+                            <div>
+                                <v-btn outlined rounded color="#692498" @click="addToCart(wish)">
+                                    장바구니
+                                </v-btn>
+                                <v-btn class="text-white ms-1" elevation="0" color="#692498"
+                                    rounded dark @click="directPurchase(wish)"
+                                >
+                                    바로 구매
+                                </v-btn>
+                            </div>
+                        </v-card-actions>
                     </v-card>
                 </v-col>
             </v-row>
@@ -110,17 +109,6 @@ export default {
 </script>
 
 <style scoped>
-    .other-info {
-        float: right;
-    }
-
-    .directive-btn {
-        display: block;
-        margin-right: 30px;
-        margin-bottom: 30px;
-        margin-top: 30px;
-    }
-
     .empty-msg {
         margin-top: 130px;
     }
